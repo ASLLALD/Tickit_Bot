ffcf2394b06ea8144508d62ae8f67b935276e6f4
ASLLALD <45972266+ASLLALD@users.noreply.github.com>
2019-02-02 19:08:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function clean(text) {
 }
 
 const prefix = "!";
-const token = "Token Your Bot";
+const token = "NTQxMjgxNDkzOTE0MzUzNjY0.DzdLZA.2XVlXp35m5j87aXrlVf069a47-M";
 
 client.on("ready", () => {
   console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
